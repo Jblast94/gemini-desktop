661270436351efaec62fc0afbc9ed30405a347c4
Ken VanDine <ken@vandine.org>
2024-12-06 19:12:32 -0500

    Fixed tray icon and use external browser for links

--- index.js
+++ index.js
@@ -1,84 +1,83 @@
-const {
-    app,
-    BrowserWindow,
-    Tray,
-    Menu,
-    nativeImage,
-    ipcMain
-} = require('electron');
+const { app, BrowserWindow, Tray, Menu, nativeImage, ipcMain, shell } = require('electron');
+const { join } = require('path');
 
 let tray = null;
 let win = null;
+const appURL = 'https://gemini.google.com'
 
-function createWindow() {
-    const icon = nativeImage.createFromPath('/snap/ken-gemini/current/icon.png');
-    win = new BrowserWindow({
-        width: 800,
-        height: 800,
-        icon: icon,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-
-    win.removeMenu();
-
-    win.loadFile('index.html');
-
-    tray = new Tray(icon);
-    // Ignore double click events for the tray icon
-    tray.setIgnoreDoubleClickEvents(true)
-
-    const contextMenu = Menu.buildFromTemplate([{
-        label: 'Copilot',
-        click: () => focusWindow()
-    }, {
-        label: 'Quit',
-        click: () => app.quit()
-    }]);
-    tray.setTitle('Copilot');
-    tray.setToolTip('Copilot');
-    tray.setContextMenu(contextMenu);
-    tray.on('click', () => {
-        console.log("click");
-        focusWindow();
-    });
-
-    // Listen for network status updates from the renderer process
-    ipcMain.on('network-status', (event, isOnline) => {
-        console.log(`Network status: ${isOnline ? 'online' : 'offline'}`);
-        console.log("network-status changed: " + isOnline);
-        if (isOnline) {
-            win.loadURL('https://gemini.google.com');
-        } else {
-            win.loadFile('offline.html');
-        }
-    });
-}
-
-function focusWindow() {
-    console.log("focusWindow");
-    if (win.isFocused()) {
-        win.hide();
-    } else {
-        win.show();
+function createWindow () {
+  const icon = nativeImage.createFromPath(join(__dirname, 'icon.png'));
+  win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    icon: icon,
+    webPreferences: {
+      preload: join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: true,
+      sandbox: false
     }
+  });
+
+  win.removeMenu();
+
+  tray = new Tray(icon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show/Hide Gemini',
+      icon: icon,
+      click: () => {
+        if (win.isVisible()) {
+          win.hide();
+        } else {
+          win.show();
+        }
+      }
+    },
+    { type: 'separator' },
+    { label: 'Quit', role: 'quit' }
+  ]);
+
+  tray.setToolTip('Gemini');
+  tray.setContextMenu(contextMenu);
+
+  ipcMain.on('log-message', (event, message) => {
+    console.log('Log from preload: ', message);
+  });
+
+  // Open links with default browser
+  ipcMain.on('open-external-link', (event, url) => {
+    console.log('open-external-link: ', url);
+    shell.openExternal(url);
+  });
+
+  // Listen for network status updates from the renderer process
+  ipcMain.on('network-status', (event, isOnline) => {
+    console.log(`Network status: ${isOnline ? 'online' : 'offline'}`);
+    console.log("network-status changed: " + isOnline);
+    if (isOnline) {
+      win.loadURL(appURL);
+    } else {
+      win.loadFile('offline.html');
+    }
+  });
+
+  //win.loadFile(join(__dirname, 'index.html'));
+  win.loadURL(appURL);
 }
 
 app.whenReady().then(createWindow);
 
-/*
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-*/
 
 app.on('activate', () => {
-    console.log("ACTIVATE");
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  console.log("ACTIVATE");
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
